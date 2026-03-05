22c411026975c3181c320b716a3eaa120105674c
beervajoshiactowiz-netizen <beervajoshi.actowiz@gmail.com>
2026-03-05 15:56:13 +0530

    created one table and stored the data in that by using batch size of 2000

--- grabfood_database.py
+++ grabfood_database.py
@@ -2,48 +2,43 @@ import json
 import mysql.connector
 
 # batches
-def make_batches(data_list, batch_size=1000):
+def make_batches(data_list, batch_size=2000):
     for i in range(0, len(data_list), batch_size):
         yield data_list[i : i + batch_size]
 
+def create_connection():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="actowiz",
+        database="grabfood_db"
+    )
 
-def send_to_db(data):
-    conn=None
-    cursor=None
-    try:
-        conn = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="actowiz",
-            database="grabfood_db"
-        )
-        cursor = conn.cursor()
-
-        #Restaurant data table creation
-        create_restaurant_query = """
-        CREATE TABLE IF NOT EXISTS grab_restaurant (
-            merchant_id VARCHAR(100) PRIMARY KEY,
-            name VARCHAR(255),
-            cuisine VARCHAR(100),
-            timingEveryday VARCHAR(100),
-            distance FLOAT,
-            ETA INT,
-            rating FLOAT,
-            DeliveryBy VARCHAR(100),
-            DeliveryOption JSON,
-            VoteCount INT,
-            Tips JSON,
-            BuisinessType VARCHAR(50),
-            Offers JSON
-        );
-        """
-        cursor.execute(create_restaurant_query)
-
+def create_tables(cursor):
+    cursor.execute("""
+            CREATE TABLE IF NOT EXISTS pdp_data (
+                merchant_id VARCHAR(100) PRIMARY KEY,
+                name VARCHAR(255),
+                cuisine VARCHAR(100),
+                timingEveryday VARCHAR(100),
+                distance FLOAT,
+                ETA INT,
+                rating FLOAT,
+                DeliveryBy VARCHAR(100),
+                DeliveryOption JSON,
+                VoteCount INT,
+                Tips JSON,
+                BuisinessType VARCHAR(50),
+                Offers JSON,
+                menu JSON
+            );
+            """)
+def send_to_db(data,cursor,conn):
         insert_restaurant = """
-        INSERT INTO grab_restaurant
+        INSERT INTO pdp_data
         (merchant_id, name, cuisine, timingEveryday, distance,
-         ETA, rating, DeliveryBy,DeliveryOption, VoteCount, Tips, BuisinessType,Offers)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s,%s)
+         ETA, rating, DeliveryBy,DeliveryOption, VoteCount, Tips, BuisinessType,Offers,menu)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s,%s,%s)
         ON DUPLICATE KEY UPDATE
         merchant_id=VALUES(merchant_id),
         name=VALUES(name),
@@ -57,43 +52,20 @@ def send_to_db(data):
         VoteCount=VALUES(VoteCount),
         Tips=VALUES(Tips),
         BuisinessType=VALUES(BuisinessType),
-        Offers= VALUES(Offers)
+        Offers= VALUES(Offers),
+        menu=VALUES(menu)
         """
-        #Menu items data table creation
-        create_menu_query = """
-        CREATE TABLE IF NOT EXISTS menu (
-            id bigint auto_increment PRIMARY KEY,
-            item_id VARCHAR(100) unique,
-            merchant_id VARCHAR(100),
-            category_name VARCHAR(100),
-            name VARCHAR(255),
-            description TEXT,
-            price FLOAT,
-            available BOOLEAN,
-            images JSON,
-            FOREIGN KEY (merchant_id) REFERENCES grab_restaurant(merchant_id) 
-        );
-        """
-        cursor.execute(create_menu_query)
 
-        menu_insert = """
-        INSERT INTO menu
-        (item_id, merchant_id, category_name, name,
-         description, price, available, images)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-        ON DUPLICATE KEY UPDATE
-        item_id=VALUES(item_id)
-        """
 
         restaurant_list=[]
-        menu_list=[]
+
         for restaurant in data:
-            merchantId = restaurant.get("merchant_id")
-            if not merchantId:
+            m_id = restaurant.get("merchant_id")
+            if not m_id:
                 continue
 
             restaurant_list.append( (
-                merchantId,
+                m_id,
                 restaurant.get("name"),
                 restaurant.get("cuisine"),
                 restaurant.get("timingEveryday"),
@@ -105,45 +77,12 @@ def send_to_db(data):
                 restaurant.get("VoteCount"),
                 json.dumps(restaurant.get("Tips", [])),
                 restaurant.get("BuisinessType"),
-                json.dumps(restaurant.get("Offers", []))
+                json.dumps(restaurant.get("Offers", [])),
+                json.dumps(restaurant.get("menu",[]))
             ))
 
-            # Nested loop for menu
-            for category in restaurant.get("menu", []):
-                category_name = category.get("category_name")
-
-                for item in category.get("items", []):
-                    menu_list.append( (
-                        item.get("item_id"),
-                        restaurant.get("merchant_id"),
-                        category_name,
-                        item.get("name"),
-                        item.get("description"),
-                        item.get("price_display"),
-                        item.get("available"),
-                        json.dumps(item.get("images", []))
-                    ))
-
-        for batch in make_batches(restaurant_list, 1000):
+        for batch in make_batches(restaurant_list, 2000):
             cursor.executemany(insert_restaurant, batch)
             conn.commit()
+
         print(f"Processed {len(restaurant_list)} restaurants.")
-
-        # Executing Batches for Menu Items
-        for batch in make_batches(menu_list, 1000):
-            cursor.executemany(menu_insert, batch)
-            conn.commit()
-        print(f"Processed {len(menu_list)} menu items.")
-
-    except mysql.connector.Error as err:
-        if conn:
-            conn.rollback()
-        print("Error occurred: ",err)
-
-    finally:
-        if cursor:
-            cursor.close()
-        if conn:
-            conn.close()
-
-
